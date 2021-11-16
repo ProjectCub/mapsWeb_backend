ea28ff7f87d2a5d547c2d029fe44bfba1dd27fc1
inciner8r <punarvpawade17@gmail.com>
2021-11-17 00:36:39 +0530

    nodemon and shitz[temp]

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-// http is an inbuilt module in Node.js
-const http = require("http");
+const express = require("express");
+const app = express();
 
-// createServer method takes a callback function as argument
-// the callback function takes two arguments req and re
-const server = http.createServer(function (req, res) {
-  res.statusCode = 200; // 200 = OK
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Demo page</h1>");
-  res.end();
+app.get("/", (req, res) => {
+  res.send("hello world ");
+  console.log("new user");
 });
 
-// server is listening to incoming requests on port 3000 on localhost
-server.listen(3000, function () {
-  console.log("Listening on port http://localhost:3000");
-});
+app.listen(2000, () => console.log("2000 started"));
